7d172ceec7269f58e1d1869c72fb8436ac68e93e
vly <lvince421@gmail.com>
2024-07-21 14:56:37 -0700

    finalized animal-profile

--- server/models/animal-profiles.mjs
+++ server/models/animal-profiles.mjs
@@ -18,7 +18,6 @@ db.once("open", (err) => {
 });
 
 // Defines Animal Profile Schema
-<<<<<<< HEAD
 const animalProfilesSchema = mongoose.Schema({
     animalName: {type: String, required: true, unique: true},
     type: {type: String, required: true},                   // dog, cat, other
@@ -26,21 +25,11 @@ const animalProfilesSchema = mongoose.Schema({
     disposition: {type: String, required: true},            // "Good with other animals", "Good with children", "Animal must be leashed at all times"
     isAvailable: {type: Boolean, require: true},
     dateCreated: {type: Date, default : Date.Now, require: true},
-=======
-const animalSchema = mongoose.Schema({
-    name: {type: String, required: true, unique: true},
-    type: {type: String, required: true},                   // dog, cat, other
-    breed: {type: String, required: true},                  // most common ones + "Other"
-    disposition: {type: String, required: true},            // "Good with other animals", "Good with children", "Animal must be leashed at all times"
-    dateCreated: {type: Date, required: true},
-    isAvailable: {type: Boolean, require: true},
->>>>>>> 0950a40 (create animal-profile)
     createByUserId: {type: String, required: true}
 }, {
     versionKey: false
 });
 
-<<<<<<< HEAD
 // Animal Profile Model
 const animalProfileModel = mongoose.model("AnimalProfiles", animalProfilesSchema);
 
@@ -96,61 +85,24 @@ const updateAnimalById = async (id, animalName, type, breed, disposition, isAvai
         disposition: disposition,
         isAvailable: isAvailable,
         dateCreated: dateCreated,
-=======
-
-// Models
-const animalProfileModel = mongoose.model("AnimalProfiles", animalSchema);
-
-
-// CRUD
-// Update
-const updateAnimalById = async (id, name, type, breed, disposition, isAvailable) => {
-    const updateResponse = await animalProfileModel.replaceOne({_id: id}, {
-        name: name,
-        type: type,
-        breed: breed,
-        disposition: disposition,
-        dateCreated: dateCreated,
-        isAvailable: isAvailable,
->>>>>>> 0950a40 (create animal-profile)
         createByUserId: createByUserId
     });
     return {
         id: id,
-<<<<<<< HEAD
         animalName: animalName,
         type: type,
         breed: breed,
         disposition: disposition,
         isAvailable: isAvailable,
         dateCreated: dateCreated,
-=======
-        name: name,
-        type: type,
-        breed: breed,
-        disposition: disposition,
-        dateCreated: dateCreated,
-        isAvailable: isAvailable,
->>>>>>> 0950a40 (create animal-profile)
         createByUserId: createByUserId
     }
 };
 
 // Delete
 const deleteAnimalById = async(id) => {
-<<<<<<< HEAD
     return await animalProfileModel.deleteOne({_id: id}).exec();
 }
 
 
 export {createAnimalProfile, getAllAnimalProfiles, getAnimalProfileByName, getAnimalProfileByID, updateAnimalById, deleteAnimalById};
-=======
-    const deleteResponse = await animalProfileModel.deleteOne({_id: id});
-    return deleteResponse.deletedCount;
-}
-
-
-
-
-export {updateAnimalById, deleteAnimalById};
->>>>>>> 0950a40 (create animal-profile)
