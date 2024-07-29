c59de13beebbb7ef75514d1c913e260a78f67908
Sophia Lilienthal <sophialilienthal@icloud.com>
2024-07-29 12:52:48 -0600

    Updated build directory, added build legacy

---
+++ client/build/precache-manifest.72979232ce11a6e23c2a5d9e86040a54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294ba26d95624e93253da88834ad25ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8f7cd8316c13dffb788",
+    "url": "/static/css/main.b4bb053a.chunk.css"
+  },
+  {
+    "revision": "7a0a0bc066466823a241",
+    "url": "/static/js/2.3ea23f8a.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.3ea23f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c434f1e0c0c22e4d1d",
+    "url": "/static/js/3.e866083c.chunk.js"
+  },
+  {
+    "revision": "f8f7cd8316c13dffb788",
+    "url": "/static/js/main.7e6c2f6f.chunk.js"
+  },
+  {
+    "revision": "b6c5b15aba006a604c97",
+    "url": "/static/js/runtime-main.c5405ecd.js"
+  }
+]);
