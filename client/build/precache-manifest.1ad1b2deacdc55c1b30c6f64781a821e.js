622e9eaf4007e6be1a31cdec878f8df700d28828
vly <lvince421@gmail.com>
2024-08-07 15:09:00 -0700

    rebuilt

---
+++ client/build/precache-manifest.1ad1b2deacdc55c1b30c6f64781a821e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7168aea0e5fe82bd702196abb76f724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "598dbc6cca7d380fd602",
+    "url": "/static/css/main.fd298633.chunk.css"
+  },
+  {
+    "revision": "bdb9322904d1e25ad6f4",
+    "url": "/static/js/2.074c031f.chunk.js"
+  },
+  {
+    "revision": "70cd410627b561ddc4ec197aaaa6e425",
+    "url": "/static/js/2.074c031f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c1022694c5ed50a7636",
+    "url": "/static/js/3.bddfce68.chunk.js"
+  },
+  {
+    "revision": "598dbc6cca7d380fd602",
+    "url": "/static/js/main.e638719d.chunk.js"
+  },
+  {
+    "revision": "563fa3cfa077fe688629",
+    "url": "/static/js/runtime-main.140b3aab.js"
+  }
+]);
