da770a538d907bf8a78486d656076d4f5a371626
Sophia Lilienthal <sophialilienthal@icloud.com>
2024-08-09 16:33:45 -0600

    Rebuilt client

--- client/build/precache-manifest.6fc07eec2aead048f9be9d6ed9e041f9.js
+++ client/build/precache-manifest.6fc07eec2aead048f9be9d6ed9e041f9.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "37b9c4301124236145d01c6e0ce73991",
+    "revision": "d5a446cfa9947e8a268ec8e68e889453",
     "url": "/index.html"
   },
   {
-    "revision": "0492bf14225f0adb0203",
+    "revision": "8637c2cc6c850aadabbb",
     "url": "/static/css/main.7bfa032e.chunk.css"
   },
   {
-    "revision": "34789233d8a68789971f",
-    "url": "/static/js/2.7431d33f.chunk.js"
+    "revision": "36a879a978b84878362c",
+    "url": "/static/js/2.65e6ba21.chunk.js"
   },
   {
     "revision": "70cd410627b561ddc4ec197aaaa6e425",
-    "url": "/static/js/2.7431d33f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.65e6ba21.chunk.js.LICENSE.txt"
   },
   {
     "revision": "4c1022694c5ed50a7636",
     "url": "/static/js/3.bddfce68.chunk.js"
   },
   {
-    "revision": "0492bf14225f0adb0203",
-    "url": "/static/js/main.fc946a56.chunk.js"
+    "revision": "8637c2cc6c850aadabbb",
+    "url": "/static/js/main.3aad2a6d.chunk.js"
   },
   {
     "revision": "563fa3cfa077fe688629",
